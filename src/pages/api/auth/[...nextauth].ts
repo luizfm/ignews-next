88d44e769c9bd41bcc37a8938cfbae76a73ce4f4
luizfm <luiz.ado@hotmail.com>
2022-03-23 16:26:58 -0300

    feat: Add faunadb into project

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import Github from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -12,5 +15,43 @@ export default NextAuth({
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    async signIn(params, account, profile) {
+      const { email } = params.user
+      console.log(email)
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: { email }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
