ee1efa7240619da869ddaa2e5764db1614ec518c
luizfm <luiz.ado@hotmail.com>
2022-03-29 18:12:06 -0300

    feat: Add prismic integration and active link component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,26 @@
-import Avatar from '../../../public/icons/avatar.svg'
+import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import styles from './styles.module.scss'
 import { SignInButton } from '../sing-in-button'
+import { ActiveLink } from '../active-link'
 
 export function Header() {
+  const { asPath } = useRouter()
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src="/icons/logo.svg" alt="Application logo" height={31} width={110} />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
